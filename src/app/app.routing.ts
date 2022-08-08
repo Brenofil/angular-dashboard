b951d95855a22738ae8c17012b922fa25fe159ff
breno-fil <breno.filgueiras@uniriotec.br>
2022-08-08 01:14:22 -0300

    brenofil - changes in app.routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,20 +1,30 @@
 import { Routes } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { DefaultLayoutModule } from './layouts/default-layout/default-layout.module';
 
 export const AppRoutes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full',
-  }, {
-    path: '',
+  }, 
+  {
+    path: '/admin',
     component: AdminLayoutComponent,
     children: [
       {
         path: '',
         loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule)
       }]
+  },
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      {path: '', loadChildren: () => import('./layouts/default-layout/default-layout.module').then(x => x.DefaultLayoutModule)}
+    ]
   },
   {
     path: '**',
